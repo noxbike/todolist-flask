ead5a253370f6a52580d32f20b8d89aa0bcfae79
noxbike <noxbike@gmail.com>
2025-03-22 14:14:06 +0400

    sqlalchemy installation and import

--- todolist/__init__.py
+++ todolist/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app  = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todolist.db'
+db = SQLAlchemy(app)
 
 from todolist import routes
